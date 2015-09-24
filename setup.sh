8186bfa98001828fd10d1e1d7c4193b530bdb65a
Andrey Belokrylov <bel@bel-mbp.local>
2015-09-23 17:21:09 -0700

    adding bin scripts

--- setup.sh
+++ setup.sh
@@ -9,4 +9,5 @@ git clone https://github.com/biokrylov/MyDotFiles.git
 
 ln -sf MyDotFiles/.bash_aliases .
 ln -sf MyDotFiles/.bash_profile .
+ln -sf MyDotFiles/bin/* bin/
 echo "\nsource .bash_aliases\n" >> .bashrc 
