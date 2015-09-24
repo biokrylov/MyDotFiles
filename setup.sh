c64f3273b19bbe7e717c08ce71d4b2ed53365bc5
biokrylov <andrey.belokrylov@gmail.com>
2015-09-23 17:47:15 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,11 +5,11 @@ if [ -d ./MyDotFiles/ ]; then
     mv MyDotFiles MyDotFiles.old
 fi
 
+git clone https://github.com/biokrylov/MyDotFiles.git
+
 if [ ! -d ./bin/ ]; then
     mkdir bin
 fi
-
-git clone https://github.com/biokrylov/MyDotFiles.git
 
 ln -sf MyDotFiles/.bash_aliases .
 ln -sf MyDotFiles/.bash_profile .
