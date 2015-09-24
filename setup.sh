6f8f39e0fbe926c480e0e936e54dc0f49b3bf3c4
biokrylov <andrey.belokrylov@gmail.com>
2015-09-23 17:38:39 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,5 +10,5 @@ git clone https://github.com/biokrylov/MyDotFiles.git
 ln -sf MyDotFiles/.bash_aliases .
 ln -sf MyDotFiles/.bash_profile .
 ln -sf MyDotFiles/bin/* bin/
-echo "\nsource .bash_aliases\n" >> .bashrc 
+printf "\nsource .bash_aliases\n" >> .bashrc 
 echo "setup complete"
